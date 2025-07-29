2bb35fb27004b4c25b0e2ce3becb87d860aaf4b7
Peawills <115140417+Peawills@users.noreply.github.com>
2025-07-29 16:10:32 +0100

    Fix sQL

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,10 +1,9 @@
 import mysql.connector
-from mysql.connector import Error
 
 
 def create_database():
     try:
-        # Connect to MySQL server (update user and password as needed)
+        # Connect to MySQL server
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
@@ -16,14 +15,16 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
-        print(f"Error while connecting to MySQL: {e}")
+    except mysql.connector.Error as err:
+        print(f"Error while connecting to MySQL: {err}")
 
     finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-            # print("MySQL connection is closed")
+        try:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+        except NameError:
+            pass  # connection was never established
 
 
 if __name__ == "__main__":
